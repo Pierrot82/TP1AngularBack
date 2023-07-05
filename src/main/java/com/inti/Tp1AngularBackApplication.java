1cdcd2de0b6676c59c5e20eb0823e90b209018ae
Pierrot82 <pierre.larroque.82@hotmail.fr>
2023-07-05 15:19:00 +0200

    Creation projet

---
+++ src/main/java/com/inti/Tp1AngularBackApplication.java
@@ -0,0 +1,13 @@
+package com.inti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp1AngularBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp1AngularBackApplication.class, args);
+	}
+
+}
