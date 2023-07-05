49799aace5263d6dc217d11b2e8d78241531b867
Pierrot82 <pierre.larroque.82@hotmail.fr>
2023-07-05 21:27:44 +0200

    RestController pour ConcertController et LieuController

--- src/main/java/com/inti/controller/ConcertController.java
+++ src/main/java/com/inti/controller/ConcertController.java
@@ -1,70 +1,90 @@
 package com.inti.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 
 import com.inti.model.Concert;
 import com.inti.model.Soliste;
 import com.inti.repository.IConcertRepository;
 
-@Controller
-@RequestMapping("concert")
+@RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class ConcertController {
 	
 	@Autowired
 	IConcertRepository icr;
 	
-	@GetMapping("creerConcert")
-	public String formConcert() {
-		return "creerConcert";
-	}
 
 	@PostMapping("saveConcert")
-	public String saveConcert(@ModelAttribute("concert") Concert s) {
-		icr.save(s);
-		return "redirect:/concert/listeConcert";
+	public boolean saveConcert(@RequestBody Concert concert) {
+		try {
+			icr.save(concert);
+			return true;
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return false;
 	}
 
 	@GetMapping("listeConcert")
-	public String listeConcert(Model m) {
-		m.addAttribute("listeConcert", icr.findAll().toArray());
-		return "listeConcert";
-	}
-	@GetMapping("deleteConcert/{numeroConcert}")
-	public String deleteConcert(@PathVariable("numeroConcert") int numeroConcert) {
-		icr.deleteById(numeroConcert);
-		return "redirect:/concert/listeConcert";
-	}
-	
-
-	@GetMapping("modifierConcert/{numeroConcert}")
-	public String modifierConcert(@PathVariable("numeroConcert") int numeroConcert, Model m)
-	{
-		m.addAttribute("concert", icr.findById(numeroConcert).get());
-		return "modifierConcert";
-	}
-	
-	@PostMapping("modifierConcert/updateConcert")
-	public String updateConcert(@ModelAttribute("concert") Concert s)
-	{
-		icr.save(s);
-		return "redirect:/concert/listeConcert";
+	public List<Concert> listeConcert() {
+		return icr.findAll();
 	}
 	
 	
-	@GetMapping("getConcert/{numeroConcert}")
-	public String getConcert(@PathVariable("numeroConcert") int numeroConcert, Model m)
-	{
-		m.addAttribute("c1", icr.findById(numeroConcert).get());
+	@GetMapping("deleteConcert/{id}")
+	public boolean deleteConcert(@PathVariable("id") int id) {
 		
-		return "getConcert";
+		try {
+			Concert c = icr.getReferenceById(id);
+			System.out.println(c); //permet de déclencher l'exception retourné par getReferenceById()
+			icr.delete(c);
+			return true;
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return false;
+	}	
+	
+	@GetMapping("getConcert/{id}")
+	public Concert getConcert(@PathVariable("id") int id)
+	{
+		Concert c = null;
+		try {
+			c = icr.findById(id).get();
+			return c;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return c;
+	}
+	
+	@PostMapping("modifierConcert")
+	public boolean modifierConcert(@RequestBody Concert concert)
+	{
+		try {
+			icr.save(concert);
+			return true;
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return false;
 	}
 	
 
--- src/main/java/com/inti/controller/LieuController.java
+++ src/main/java/com/inti/controller/LieuController.java
@@ -1,78 +1,88 @@
 package com.inti.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.inti.model.Lieu;
 import com.inti.repository.ILieuRepository;
 
-@Controller
-@RequestMapping("lieu")
+@RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class LieuController {
 	
 	@Autowired
 	ILieuRepository ilr;
 	
-	@GetMapping("creerLieu")
-	public String formLieu() {
-		return "creerLieu";
-	}
 
 	@PostMapping("saveLieu")
-	public String saveLieu(@ModelAttribute("lieu") Lieu l) {
-		ilr.save(l);
-		//return "redirect:/listeLieu";
-		return "redirect:/lieu/listeLieu";
-		
-		
+	public boolean saveLieu(@RequestBody Lieu lieu) {
+		try {
+			ilr.save(lieu);
+			return true;
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return false;
 	}
 
 	@GetMapping("listeLieu")
-	public String listeLieu(Model m) {
-		
-		m.addAttribute("listeLieu", ilr.findAll());
-		
-		return "listeLieu";
+	public List<Lieu> listelieu() {
+		return ilr.findAll();
 	}
+	
+	
+	@GetMapping("deleteLieu/{id}")
+	public boolean deleteLieu(@PathVariable("id") int id) {
+		
+		try {
+			Lieu l = ilr.getReferenceById(id);
+			System.out.println(l); //permet de déclencher l'exception retourné par getReferenceById()
+			ilr.delete(l);
+			return true;
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return false;
+	}	
 	
 	@GetMapping("getLieu/{id}")
-	public String getLieu(@PathVariable("id") int id, Model m) {
-		
-		
-		m.addAttribute("lieu1", ilr.findById(id).get());
-		
-		return "getLieu";
-	}
-	
-
-
-	
-	@GetMapping("deleteLieu/{codeLieu}")
-	public String deleteLieu(@PathVariable("codeLieu") int codeLieu) {
-
-		ilr.deleteById(codeLieu);
-		return "redirect:/lieu/listeLieu";
-	}
-	
-	@GetMapping("modifierLieu/{codeLieu}")
-	public String modifierLieu(@PathVariable("codeLieu") int codeLieu, Model m)
+	public Lieu getLieu(@PathVariable("id") int id)
 	{
-		m.addAttribute("lieu", ilr.findById(codeLieu).get());
-		return "modifierLieu";
+		Lieu l = null;
+		try {
+			l = ilr.findById(id).get();
+			return l;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return l;
 	}
 	
-	@PostMapping("modifierLieu/updateLieu")
-	public String updateLieu(@ModelAttribute("lieu") Lieu l)
+	@PostMapping("modifierLieu")
+	public boolean modifierLieu(@RequestBody Lieu lieu)
 	{
-		ilr.save(l);
-		return "redirect:/lieu/listeLieu";
+		try {
+			ilr.save(lieu);
+			return true;
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return false;
 	}
 
 }
